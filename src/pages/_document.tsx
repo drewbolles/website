b49bd586dbbb74344ef8633d4d45d576937a96cd
Drew Bolles <drewbolles@gmail.com>
2020-12-18 14:56:24 -0500

    Add full favicon setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,26 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <Manifest href="/manifest.json" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+          <meta name="msapplication-TileColor" content="#2d89ef" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
