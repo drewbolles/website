2ce3538e863c3423f217836e37bf0d5ca3020b12
Drew Bolles <drewbolles@gmail.com>
2020-12-18 21:37:35 -0500

    add google analytics via gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
+import * as gtag from '../utils/gtag';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
